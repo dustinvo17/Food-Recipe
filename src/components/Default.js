45a20cde12ee30c2329dd86ce6758f813dec3b4c
dustinvo17 <46255218+dustinvo17@users.noreply.github.com>
2019-07-31 02:09:16 -0500

    final

--- src/components/Default.js
+++ src/components/Default.js
@@ -4,7 +4,7 @@ export default class Default extends Component {
     render() {
         return (
             <div className="ui header huge">
-                404 Page Not Found
+                Page Not Found
                 URL {this.props.location.pathname} 
             </div>
         )
